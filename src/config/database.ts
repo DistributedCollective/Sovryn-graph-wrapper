bb18a090dc8698292080e837a0ec6de71b08c0ca
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-05-18 16:12:25 +0300

    Correct migration config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,18 +1,18 @@
-import { ConnectionOptions } from "typeorm";
-import { LiquidityPoolSummary, Asset, Tvl, IlliquidSov } from "../entity";
+import { ConnectionOptions } from 'typeorm'
+import { LiquidityPoolSummary, Asset, Tvl, IlliquidSov } from '../entity'
 
-import config, { Environment } from "./config";
+import config, { Environment } from './config'
 const {
   postgresHost,
   postgresPort,
   postgresUser,
   postgresPassword,
   postgresDatabase,
-  env,
-} = config;
+  env
+} = config
 
 const dbConfig: ConnectionOptions = {
-  type: "postgres",
+  type: 'postgres',
   host: postgresHost,
   port: postgresPort,
   username: postgresUser,
@@ -21,9 +21,9 @@ const dbConfig: ConnectionOptions = {
   entities: [LiquidityPoolSummary, Asset, Tvl, IlliquidSov],
   synchronize: env === Environment.Development,
   cli: {
-    migrationsDir: "src/migration",
+    migrationsDir: 'src/migration'
   },
-  migrations: ["src/migration/**/*.ts"],
-};
+  migrations: ['src/migration/**/*.ts']
+}
 
-export default dbConfig;
+export default dbConfig
