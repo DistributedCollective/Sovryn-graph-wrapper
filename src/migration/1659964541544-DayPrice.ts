2c38719b8792c593770f92465baf896a2b77272e
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-08-08 14:16:40 +0100

    Lint

--- src/migration/1659964541544-DayPrice.ts
+++ src/migration/1659964541544-DayPrice.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class DayPrice1659964541544 implements MigrationInterface {
-    name = 'DayPrice1659964541544'
+  name = 'DayPrice1659964541544'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "liquidity_pool_summary" ADD "dayPrice" numeric(40,18) NOT NULL DEFAULT '0'`);
-    }
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE "liquidity_pool_summary" ADD "dayPrice" numeric(40,18) NOT NULL DEFAULT \'0\'')
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "liquidity_pool_summary" DROP COLUMN "dayPrice"`);
-    }
-
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE "liquidity_pool_summary" DROP COLUMN "dayPrice"')
+  }
 }
