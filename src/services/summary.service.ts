9ce8c06239c66f7f06ae75da3d0884aa43ed1d4d
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-08-09 10:43:50 +0100

    Bug fixes for dayPrice

--- src/services/summary.service.ts
+++ src/services/summary.service.ts
@@ -3,90 +3,90 @@
  *
  */
 
-import { priceAndVolumeQuery } from "../graphQueries/priceAndVolume";
-import { candlestickQuery } from "../graphQueries/candlesticks";
-import { blockNumberFromTimestamp } from "../graphQueries/block";
-import { getQuery } from "../utils/apolloClient";
-import { isNil } from "lodash";
-import { bignumber } from "mathjs";
-import { createMultipleSummaryPairData } from "../models/summary.model";
+import { priceAndVolumeQuery } from '../graphQueries/priceAndVolume'
+import { candlestickQuery } from '../graphQueries/candlesticks'
+import { blockNumberFromTimestamp } from '../graphQueries/block'
+import { getQuery } from '../utils/apolloClient'
+import { isNil } from 'lodash'
+import { bignumber } from 'mathjs'
+import { createMultipleSummaryPairData } from '../models/summary.model'
 import {
   Transaction,
   LiquidityPool,
   CandleStick,
-  CandleSticksInterval,
-} from "../generated-schema";
-import { ITradingPairDataBase, ITradingPairData } from "../utils/interfaces";
-import log from "../logger";
+  CandleSticksInterval
+} from '../generated-schema'
+import { ITradingPairDataBase, ITradingPairData } from '../utils/interfaces'
+import log from '../logger'
 
-const logger = log.logger.child({ module: "Summary" });
+const logger = log.logger.child({ module: 'Summary' })
 
 interface LiquidityPoolData {
-  liquidityPools: LiquidityPool[];
+  liquidityPools: LiquidityPool[]
 }
 
-const DAY_MILLISECONDS = 24 * 60 * 60 * 1000;
-const WEEK_MILLISECONDS = 24 * 60 * 60 * 7 * 1000;
+const DAY_MILLISECONDS = 24 * 60 * 60 * 1000
+const WEEK_MILLISECONDS = 24 * 60 * 60 * 7 * 1000
 
-export default async function main(): Promise<void> {
-  logger.info("Running Summary Data main function");
+export default async function main (): Promise<void> {
+  logger.info('Running Summary Data main function')
   try {
-    logger.debug("Getting block numbers");
+    logger.debug('Getting block numbers')
     const yesterdayBlock = await getBlockNumberFromDate(
       Math.floor((new Date().getTime() - DAY_MILLISECONDS) / 1000)
-    );
+    )
     const lastWeekBlock = await getBlockNumberFromDate(
       Math.floor((new Date().getTime() - WEEK_MILLISECONDS) / 1000)
-    );
+    )
 
-    logger.debug("Getting liquidity pool data");
+    logger.debug('Getting liquidity pool data')
     const currentData: LiquidityPoolData = await getQuery(
       priceAndVolumeQuery()
-    );
+    )
 
     const dayData: LiquidityPoolData = await getQuery(
       priceAndVolumeQuery(yesterdayBlock)
-    );
+    )
     const weekData: LiquidityPoolData = await getQuery(
       priceAndVolumeQuery(lastWeekBlock)
-    );
+    )
 
-    logger.debug("Sorting data");
+    logger.debug('Sorting data')
     const sortedData = sortByPairs(
       currentData.liquidityPools,
       dayData.liquidityPools,
       weekData.liquidityPools
-    );
+    )
 
-    logger.debug("Parsing data");
-    const parsedData = parseData(sortedData);
-    logger.debug("Adding candlestick data");
-    const output: ITradingPairData[] = [];
+    logger.debug('Parsing data')
+    const parsedData = parseData(sortedData)
+    logger.debug('Adding candlestick data')
+    const output: ITradingPairData[] = []
     for (const i of parsedData) {
       const highLowPrices = await getHighAndLowPrices(
         i.baseId,
         i.lastPrice,
         i.lastPriceUsd
-      );
+      )
       const item = {
         ...i,
-        ...highLowPrices,
-      };
-      output.push(item);
+        ...highLowPrices
+      }
+      output.push(item)
     }
 
-    logger.debug("Saving data");
-    await createMultipleSummaryPairData(output);
-    logger.info("Summary Data main function completed");
+    logger.debug('Saving data')
+    await createMultipleSummaryPairData(output)
+    logger.info('Summary Data main function completed')
   } catch (e) {
-    logger.error(e as Error, "Error running summary main data");
+    logger.error(e as Error, 'Error running summary main data')
   }
 }
 
-async function getBlockNumberFromDate(timestamp: number): Promise<number> {
-  const data = await getQuery(blockNumberFromTimestamp(timestamp));
-  const transactions = data.transactions as Transaction[];
-  return parseInt(transactions[0].blockNumber);
+async function getBlockNumberFromDate (timestamp: number): Promise<number> {
+  const data = await getQuery(blockNumberFromTimestamp(timestamp))
+  const transactions = data.transactions as Transaction[]
+  return parseInt(transactions[0].blockNumber)
 }
 
 const sortByPairs = (
@@ -94,76 +94,76 @@ const sortByPairs = (
   dayData: LiquidityPool[],
   weekData: LiquidityPool[]
 ): Array<{
-  currentData: LiquidityPool;
-  dayData: LiquidityPool;
-  weekData: LiquidityPool;
+  currentData: LiquidityPool
+  dayData: LiquidityPool
+  weekData: LiquidityPool
 }> =>
   currentData.map((item) => {
-    const itemDayData = dayData.find((dayItem) => dayItem.id === item.id);
-    const itemWeekData = weekData.find((weekItem) => weekItem.id === item.id);
+    const itemDayData = dayData.find((dayItem) => dayItem.id === item.id)
+    const itemWeekData = weekData.find((weekItem) => weekItem.id === item.id)
 
     return {
       currentData: item,
       dayData: !isNil(itemDayData) ? itemDayData : item,
-      weekData: !isNil(itemWeekData) ? itemWeekData : item,
-    };
-  });
+      weekData: !isNil(itemWeekData) ? itemWeekData : item
+    }
+  })
 
 const parseData = (
   sorted: Array<{
-    currentData: LiquidityPool;
-    dayData: LiquidityPool;
-    weekData: LiquidityPool;
+    currentData: LiquidityPool
+    dayData: LiquidityPool
+    weekData: LiquidityPool
   }>
 ): ITradingPairDataBase[] => {
   return sorted.map((item) => {
-    const currentData = item.currentData;
-    const dayData = item.dayData;
-    const weekData = item.weekData;
+    const currentData = item.currentData
+    const dayData = item.dayData
+    const weekData = item.weekData
 
-    let currentBaseVolume = 0;
-    let currentQuoteVolume = 0;
-    let dayBaseVolume = 0;
-    let dayQuoteVolume = 0;
+    let currentBaseVolume = 0
+    let currentQuoteVolume = 0
+    let dayBaseVolume = 0
+    let dayQuoteVolume = 0
 
     const isBaseTokenConnector0 =
       currentData.connectorTokens[0].token.symbol ===
-      currentData?.token1?.symbol;
+      currentData?.token1?.symbol
 
     currentBaseVolume = parseFloat(
       isBaseTokenConnector0
         ? currentData.connectorTokens[0].totalVolume
         : currentData.connectorTokens[1].totalVolume
-    );
+    )
     currentQuoteVolume = parseFloat(
       isBaseTokenConnector0
         ? currentData.connectorTokens[1].totalVolume
         : currentData.connectorTokens[0].totalVolume
-    );
+    )
     dayBaseVolume = parseFloat(
       isBaseTokenConnector0
         ? dayData.connectorTokens[0].totalVolume
         : dayData.connectorTokens[1].totalVolume
-    );
+    )
     dayQuoteVolume = parseFloat(
       isBaseTokenConnector0
         ? dayData.connectorTokens[1].totalVolume
         : dayData.connectorTokens[0].totalVolume
-    );
+    )
 
-    const lastPriceBtc = currentData?.token1?.lastPriceBtc;
-    const lastPriceUsd = currentData?.token1?.lastPriceUsd;
-    const baseTokenSymbol = currentData.token1?.symbol;
-    const baseTokenId = currentData.token1?.id;
-    const quoteTokenSymbol = currentData.token0?.symbol;
-    const quoteTokenId = currentData.token0?.id;
+    const lastPriceBtc = currentData?.token1?.lastPriceBtc
+    const lastPriceUsd = currentData?.token1?.lastPriceUsd
+    const baseTokenSymbol = currentData.token1?.symbol
+    const baseTokenId = currentData.token1?.id
+    const quoteTokenSymbol = currentData.token0?.symbol
+    const quoteTokenId = currentData.token0?.id
 
     return {
       poolId: currentData.id,
-      baseSymbol: !isNil(baseTokenSymbol) ? baseTokenSymbol : "",
-      baseId: !isNil(baseTokenId) ? baseTokenId : "",
-      quoteSymbol: !isNil(quoteTokenSymbol) ? quoteTokenSymbol : "",
-      quoteId: !isNil(quoteTokenId) ? quoteTokenId : "",
+      baseSymbol: !isNil(baseTokenSymbol) ? baseTokenSymbol : '',
+      baseId: !isNil(baseTokenId) ? baseTokenId : '',
+      quoteSymbol: !isNil(quoteTokenSymbol) ? quoteTokenSymbol : '',
+      quoteId: !isNil(quoteTokenId) ? quoteTokenId : '',
       baseVolume24h: currentBaseVolume - dayBaseVolume,
       quoteVolume24h: currentQuoteVolume - dayQuoteVolume,
       lastPrice: !isNil(lastPriceBtc) ? parseFloat(lastPriceBtc) : 0,
@@ -187,65 +187,65 @@ const parseData = (
       /** dayPrice is the BTC price 24 hours ago. This is used by frontend  */
       dayPrice: !isNil(dayData?.token1)
         ? parseFloat(dayData?.token1?.lastPriceBtc)
-        : 100,
-    };
-  });
-};
+        : 100
+    }
+  })
+}
 
-function calculatePriceChange(
+function calculatePriceChange (
   currentPrice: string | undefined,
   prevPrice: string | undefined
 ): number {
   if (isNil(currentPrice) || isNil(prevPrice)) {
-    return 0;
+    return 0
   }
-  const delta = bignumber(currentPrice).minus(parseFloat(prevPrice));
-  const percentChange = delta.div(parseFloat(currentPrice)).mul(100);
-  return parseFloat(percentChange.toFixed(2));
+  const delta = bignumber(currentPrice).minus(parseFloat(prevPrice))
+  const percentChange = delta.div(parseFloat(currentPrice)).mul(100)
+  return parseFloat(percentChange.toFixed(2))
 }
 
 /** Retrieves previous 24 hours of candlesticks to get high/low */
-async function getHighAndLowPrices(
+async function getHighAndLowPrices (
   baseToken: string,
   currentBtcPrice: number,
   currentUsdPrice: number
 ): Promise<{
-  highUsd: number;
-  lowUsd: number;
-  highBtc: number;
-  lowBtc: number;
-}> {
+    highUsd: number
+    lowUsd: number
+    highBtc: number
+    lowBtc: number
+  }> {
   const yesterdayTimestamp = Math.floor(
     (new Date().getTime() - DAY_MILLISECONDS) / 1000
-  );
+  )
   const query = candlestickQuery(
     baseToken,
     CandleSticksInterval.HourInterval,
     yesterdayTimestamp
-  );
-  const queryData = await getQuery(query);
-  let highUsd: number = currentUsdPrice;
-  let lowUsd: number = currentUsdPrice;
-  let highBtc: number = currentBtcPrice;
-  let lowBtc: number = currentBtcPrice;
+  )
+  const queryData = await getQuery(query)
+  let highUsd: number = currentUsdPrice
+  let lowUsd: number = currentUsdPrice
+  let highBtc: number = currentBtcPrice
+  let lowBtc: number = currentBtcPrice
   if (!isNil(queryData) && !isNil(queryData.candleSticks)) {
-    const candlesticks = queryData.candleSticks as CandleStick[];
+    const candlesticks = queryData.candleSticks as CandleStick[]
     for (const i of candlesticks) {
-      if (i.quoteToken?.symbol === "XUSD") {
-        if (lowUsd === 0) lowUsd = parseFloat(i.low);
+      if (i.quoteToken?.symbol === 'XUSD') {
+        if (lowUsd === 0) lowUsd = parseFloat(i.low)
         if (parseFloat(i.high) > highUsd) {
-          highUsd = parseFloat(i.high);
+          highUsd = parseFloat(i.high)
         }
         if (parseFloat(i.low) < lowUsd) {
-          lowUsd = parseFloat(i.low);
+          lowUsd = parseFloat(i.low)
         }
-      } else if (i.quoteToken?.symbol === "WRBTC") {
-        if (lowBtc === 0) lowBtc = parseFloat(i.low);
+      } else if (i.quoteToken?.symbol === 'WRBTC') {
+        if (lowBtc === 0) lowBtc = parseFloat(i.low)
         if (parseFloat(i.high) > highBtc) {
-          highBtc = parseFloat(i.high);
+          highBtc = parseFloat(i.high)
         }
         if (parseFloat(i.low) < lowBtc) {
-          lowBtc = parseFloat(i.low);
+          lowBtc = parseFloat(i.low)
         }
       }
     }
@@ -254,6 +254,6 @@ async function getHighAndLowPrices(
     highUsd: highUsd,
     lowUsd: lowUsd,
     highBtc: highBtc,
-    lowBtc: lowBtc,
-  };
+    lowBtc: lowBtc
+  }
 }
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,97 +1,97 @@
 interface TradingPairDataItem {
-  trading_pairs: string;
-  base_symbol: string;
-  base_symbol_legacy: string;
-  base_id: string;
-  quote_symbol: string;
-  quote_symbol_legacy: String;
-  quote_id: string;
-  base_volume: number;
-  quote_volume: number;
-  last_price: number;
-  last_price_usd: number;
-  high_price_24h: number;
-  high_price_24h_usd: number;
-  lowest_price_24h: number;
-  lowest_price_24h_usd: number;
-  price_change_percent_24h: number;
-  price_change_percent_24h_usd: number;
-  price_change_week: number;
-  price_change_week_usd: number;
-  day_price: number;
+  trading_pairs: string
+  base_symbol: string
+  base_symbol_legacy: string
+  base_id: string
+  quote_symbol: string
+  quote_symbol_legacy: String
+  quote_id: string
+  base_volume: number
+  quote_volume: number
+  last_price: number
+  last_price_usd: number
+  high_price_24h: number
+  high_price_24h_usd: number
+  lowest_price_24h: number
+  lowest_price_24h_usd: number
+  price_change_percent_24h: number
+  price_change_percent_24h_usd: number
+  price_change_week: number
+  price_change_week_usd: number
+  day_price: number
 }
 
 export interface ITradingPairDataBase {
-  poolId: string;
-  baseSymbol: string;
-  baseId: string;
-  quoteSymbol: string;
-  quoteId: string;
-  baseVolume24h: number;
-  quoteVolume24h: number;
-  lastPrice: number;
-  lastPriceUsd: number;
-  priceChangePercent24h: number;
-  priceChangePercentWeek: number;
-  priceChangePercent24hUsd: number;
-  priceChangePercentWeekUsd: number;
-  dayPrice: number;
+  poolId: string
+  baseSymbol: string
+  baseId: string
+  quoteSymbol: string
+  quoteId: string
+  baseVolume24h: number
+  quoteVolume24h: number
+  lastPrice: number
+  lastPriceUsd: number
+  priceChangePercent24h: number
+  priceChangePercentWeek: number
+  priceChangePercent24hUsd: number
+  priceChangePercentWeekUsd: number
+  dayPrice: number
 }
 
 export interface ITradingPairData extends ITradingPairDataBase {
-  highUsd: number;
-  lowUsd: number;
-  highBtc: number;
-  lowBtc: number;
+  highUsd: number
+  lowUsd: number
+  highBtc: number
+  lowBtc: number
 }
 
 export interface Summary {
-  pairs: { [key: string]: TradingPairDataItem };
-  updated_at: Date;
-  total_volume_btc: number;
-  total_volume_usd: number;
+  pairs: { [key: string]: TradingPairDataItem }
+  updated_at: Date
+  total_volume_btc: number
+  total_volume_usd: number
 }
 
 export interface AssetData {
-  id: string;
-  symbol: string | undefined;
-  name: string | undefined;
-  circulatingSupply: number;
-  cryptoAssetId: number;
+  id: string
+  symbol: string | undefined
+  name: string | undefined
+  circulatingSupply: number
+  cryptoAssetId: number
 }
 
 interface AssetDataItem {
-  symbol: string | undefined;
-  name: string | undefined;
-  id: string;
-  unified_cryptoasset_id: number | null;
-  circulating_supply: number;
-  updated: Date;
+  symbol: string | undefined
+  name: string | undefined
+  id: string
+  unified_cryptoasset_id: number | null
+  circulating_supply: number
+  updated: Date
 }
 
 export interface Asset {
-  [key: string]: AssetDataItem;
+  [key: string]: AssetDataItem
 }
 
 export interface TvlData {
-  [key: string]: {};
-  totalBtc: number;
-  totalUsd: number;
+  [key: string]: {}
+  totalBtc: number
+  totalUsd: number
 }
 
 interface TvlBaseData {
-  total_btc: number;
-  total_usd: number;
-  updatedAt: Date;
+  total_btc: number
+  total_usd: number
+  updatedAt: Date
 }
 
 export interface ITvl extends TvlBaseData {
-  [key: string]: any;
+  [key: string]: any
 }
 
 export interface ILendingPoolApyItem {
-  supply: number;
-  supply_apr: number;
-  borrow_apr: number;
-  timestamp: Date;
+  supply: number
+  supply_apr: number
+  borrow_apr: number
+  timestamp: Date
 }
