c3c65dc1eca858f945c5637e5bc1f34e7a84ccd4
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-08-15 13:59:40 +0200

    Tvl enum migration

---
+++ src/migration/1660550354504-TvlGroupEnum.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class TvlGroupEnum1660550354504 implements MigrationInterface {
+  name = 'TvlGroupEnum1660550354504'
+
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TYPE "public"."tvl_tvlgroup_enum" RENAME TO "tvl_tvlgroup_enum_old"')
+    await queryRunner.query('CREATE TYPE "public"."tvl_tvlgroup_enum" AS ENUM(\'tvlAmm\', \'tvlLending\', \'tvlProtocol\', \'tvlSubprotocols\', \'tvlStaking\', \'tvlZero\')')
+    await queryRunner.query('ALTER TABLE "tvl" ALTER COLUMN "tvlGroup" TYPE "public"."tvl_tvlgroup_enum" USING "tvlGroup"::"text"::"public"."tvl_tvlgroup_enum"')
+    await queryRunner.query('DROP TYPE "public"."tvl_tvlgroup_enum_old"')
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE TYPE "public"."tvl_tvlgroup_enum_old" AS ENUM(\'tvlAmm\', \'tvlLending\', \'tvlProtocol\', \'tvlSubprotocols\', \'tvlStaking\')')
+    await queryRunner.query('ALTER TABLE "tvl" ALTER COLUMN "tvlGroup" TYPE "public"."tvl_tvlgroup_enum_old" USING "tvlGroup"::"text"::"public"."tvl_tvlgroup_enum_old"')
+    await queryRunner.query('DROP TYPE "public"."tvl_tvlgroup_enum"')
+    await queryRunner.query('ALTER TYPE "public"."tvl_tvlgroup_enum_old" RENAME TO "tvl_tvlgroup_enum"')
+  }
+}
