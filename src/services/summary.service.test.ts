f4faaee8ba14ef69f62499711e0ee30bc5f1c3f0
BetsyBraddock <69518422+BetsyBraddock@users.noreply.github.com>
2023-01-12 17:40:14 -0300

    Fix for high/low 24 hour bug (#47)

--- src/services/summary.service.test.ts
+++ src/services/summary.service.test.ts
@@ -1,9 +1,38 @@
 /* eslint-env jest */
-import { calculatePriceChange } from './summary.service'
+import { calculatePriceChange, getHighAndLowPrices } from './summary.service'
 
 describe('calculatePriceChange()', () => {
   it('correctly calculates price change when change is > 100%', () => {
     const res = calculatePriceChange('2', '8')
     expect(res).toBe(-75)
+  })
+})
+
+describe('getHighLowPrices()', () => {
+  it('correctly gets high prices for ETH', async () => {
+    const mainnetETH = '0x1d931bf8656d795e50ef6d639562c5bd8ac2b78f'
+    const testLowBtcPrice = 0.00001
+    const testLowUsdPrice = 1
+    const res = await getHighAndLowPrices(
+      mainnetETH,
+      testLowBtcPrice,
+      testLowUsdPrice
+    )
+    console.log(res)
+    expect(res.highUsd).toBeGreaterThan(testLowUsdPrice)
+    expect(res.highBtc).toBeGreaterThan(testLowBtcPrice)
+  })
+  it('correctly gets low prices for ETH', async () => {
+    const mainnetETH = '0x1d931bf8656d795e50ef6d639562c5bd8ac2b78f'
+    const testHighBtcPrice = 1
+    const testHighUsdPrice = 100000
+    const res = await getHighAndLowPrices(
+      mainnetETH,
+      testHighBtcPrice,
+      testHighUsdPrice
+    )
+    console.log(res)
+    expect(res.lowUsd).toBeLessThan(testHighUsdPrice)
+    expect(res.lowBtc).toBeLessThan(testHighBtcPrice)
   })
 })
