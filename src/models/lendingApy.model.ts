2e2a1cedef117286f04b3af7895bae0efc2e7786
BetsyBraddock <69518422+BetsyBraddock@users.noreply.github.com>
2023-04-11 13:37:18 +0300

    Order lending apy response by date (#58)

--- src/models/lendingApy.model.ts
+++ src/models/lendingApy.model.ts
@@ -51,6 +51,9 @@ export async function getLendingPoolApy (
       where: {
         contract: pool,
         timestamp: MoreThan(startDate)
+      },
+      order: {
+        timestamp: 'ASC'
       }
     })
     return data
