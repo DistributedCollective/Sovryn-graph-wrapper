7fb8e1a5f3a85ef8bd4cb7816c7c1fe8f9a7fbd8
BetsyBraddock <k.shall0507+mutant@gmail.com>
2023-04-22 19:12:38 +0300

    Added wildcard to deprecation route

--- src/routes/deprecated.route.ts
+++ src/routes/deprecated.route.ts
@@ -7,7 +7,7 @@ import { InputValidateError } from '../errorHandlers/baseError'
 export const router = express.Router()
 
 router.get(
-  '/datafeed/price',
+  '/datafeed/price/*',
   setCacheMiddleware(1000),
   asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -21,6 +21,30 @@ router.get(
         error: true,
         message:
           'This API endpoint has now been deprecated. To access Sovryn pricing data, please use the Sovryn Subgraph. \nDocumentation for how to replace this endpoint with subgraph data can be found here: https://github.com/DistributedCollective/Sovryn-subgraph/blob/development/docs/MigrateDatafeedEndpoint.md. \nIf you need help, please reach out to the Sovryn team via our discord: https://discord.com/channels/729675474665603133/',
+        statusCode: 410
+      }
+      res.status(410).json(response)
+    } catch (error) {
+      next(error)
+    }
+  })
+)
+
+router.get(
+  '/*',
+  setCacheMiddleware(1000),
+  asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      req.log.info(req, 'get deprecated endpoint')
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        req.log.debug(errors.array(), 'handling message errors')
+        throw new InputValidateError(errors.array())
+      }
+      const response = {
+        error: true,
+        message:
+          'This API endpoint has now been deprecated. If you need help, please reach out to the Sovryn team via our discord: https://discord.com/channels/729675474665603133/',
         statusCode: 410
       }
       res.status(410).json(response)
